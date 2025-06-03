ef227f366c1e994d66e7ff5f9eaa0293ed300c1d
epiphyte <epiphyte@vertex.link>
2025-06-03 14:52:38 -0400

    Fix ref

--- src/vertex_hatch_tools/hooks.py
+++ src/vertex_hatch_tools/hooks.py
@@ -1,9 +1,9 @@
 from hatchling.plugin import hookimpl
 
-import vertex_hatch_tools.hooks as v_hooks
+import vertex_hatch_tools.plugin as v_plugin
 
 print('vertex-hatch-tools.hooks have been imported')
 
 @hookimpl
 def hatch_register_build_hook():
-    return v_hooks.VertexBuildHook
+    return v_plugin.VertexBuildHook
